e4f2d7978bf4b35709c7c1d956aeab70c56e5c6f
Fizabanu1 <163541328+Fizabanu1@users.noreply.github.com>
2024-11-08 22:01:50 +0530

    java Project

---
+++ Atm.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Atm{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int balance=10000;deposit,withdraw;
+        
+
+    }
+}
